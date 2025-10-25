bab3a7ea5f856fb7f446be3d5f337a95cf173e9e
eddacris <jetallavache@outlook.com>
2025-10-25 18:23:53 +0700

    build: add font and css minimization and optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -7,8 +9,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.[contenthash].js',
+    assetModuleFilename: 'assets/[name][hash][ext]',
   },
-  mode: 'development',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -18,7 +21,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ],
+      },
+      {
+        test: /\.(woff2?|ttf|eot|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name]-[hash][ext]'
+        }
       },
     ],
   },
@@ -27,11 +41,27 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
     }),
   ],
+  optimization: {
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
   devServer: {
     static: './dist',
-    port: 3000,
+    port: 3003,
     open: true,
     hot: true,
   },
