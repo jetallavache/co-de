6e301931f7fdc964a0fecf45145abb89505a726e
eddacris <jetallavache@outlook.com>
2025-10-25 17:22:36 +0700

    feat: add media queries for mobile devices, implement a burger menu

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const burgerMenu = () => {
+  const burger = document.getElementById('burger');
+  const menu = document.getElementById('menu');
+  const menuItem = document.querySelectorAll('.menu__item');
+
+  const toggleMenu = () => {
+    burger.classList.toggle('header__burger_active');
+    menu.classList.toggle('header__menu_active');
+    document.body.style.overflow = menu.classList.contains('header__menu_active') ? 'hidden' : '';
+  };
+
+  const closeMenu = () => {
+    burger.classList.remove('header__burger_active');
+    menu.classList.remove('header__menu_active');
+    document.body.style.overflow = '';
+  };
+
+  burger.addEventListener('click', toggleMenu);
+  menuItem.forEach((link) => link.addEventListener('click', closeMenu));
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  burgerMenu();
+});
